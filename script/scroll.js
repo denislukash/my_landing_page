dc93dfb105d9cc1855830d495a54b4e82373c16d
Denis <atlantes.kh59@gmail.com>
2017-03-02 13:16:53 +0200

    scrolling test

--- script/scroll.js
+++ script/scroll.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let $header = $("header");
+
+let tempScrollTop, currentScrollTop = 0;
+
+$($header).scroll(function () {
+
+    console.log("scroll!");
+
+    currentScrollTop = $header.scrollTop();
+
+    if(tempScrollTop < currentScrollTop){
+        console.log("scroll down!")
+    }else if(tempScrollTop > currentScrollTop){
+        console.log("scroll up!")
+    }
+
+    tempScrollTop = currentScrollTop;
+});
